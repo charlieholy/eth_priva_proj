8b323a10caae380cddb9193bce89070b0cfaf6bf
charlieholy <357483226@qq.com>
2018-05-18 14:58:56 +0800

    enable rpc

--- priv_chain/start.bat
+++ priv_chain/start.bat
@@ -1,1 +1,1 @@
-%ETHPATH%\geth --identity "PICCetherum" --ipcdisable --rpc --rpccorsdomain "*" --datadir "%cd%\chain" --port "30303"  --rpcapi "db,eth,net,web3,personal,admin,miner" --networkid 95518 console
+%ETHPATH%\geth --identity "PICCetherum" --rpc --rpccorsdomain "*" --datadir "%cd%\chain" --port "30303"  --rpcapi "db,eth,net,web3,personal,admin,miner" --networkid 95518 console
